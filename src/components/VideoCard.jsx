49f07e9a21ed19ea68d376af4936ff86caba84f2
mustafasaied97 <mustafasaied97@gmail.com>
2023-03-19 06:23:27 +0200

    edit UI

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -64,7 +64,7 @@ const VideoCard = ({loading,video:{id:{videoId},snippet},from}) => {
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
-                    sx={{width:{xs:'100%',sm:'358px',md:'320px'},height:180 ,borderRadius:'15px' ,objectFit:'contain'}}
+                    sx={{width:{xs:'85vw',sm:'358px',md:'320px'},height:180 ,borderRadius:'15px' ,objectFit:'contain'}}//fit dim
                 />
                 :
                 <Skeleton variant="rectangular" sx={{ width:{xs:'100vw',sm:'358px',md:'320px'},  height:180 ,bgcolor: '#363738',borderRadius:'15px' }} animation="wave"/>
@@ -154,3 +154,47 @@ export default VideoCard;
 //     </Link>
 // </CardContent>
 // </Card>
+
+
+
+
+
+// <Card  sx={{width:{xs:'100%',sm:'358px',md:'320px'}, borderRadius:'15px 15px 5px 5px' ,maxWidth:358 ,boxShadow:'none',border:'0',backgroundColor:'#0f0f0f' }}>
+//             <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+//             {!loading?
+//                 <CardMedia
+//                     image={snippet?.thumbnails?.high?.url}
+//                     alt={snippet?.title}
+//                     sx={{width:{xs:'100%',sm:'358px',md:'320px'},height:180 ,borderRadius:'15px' ,objectFit:'contain'}}
+//                 />
+//                 :
+//                 <Skeleton variant="rectangular" sx={{ width:{xs:'100vw',sm:'358px',md:'320px'},  height:180 ,bgcolor: '#363738',borderRadius:'15px' }} animation="wave"/>
+
+//             }
+//             </Link>
+//             <CardContent sx={{height:'106px' }}>
+//             {!loading?
+//             <>
+//                <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+//                     <Typography variant='subtitle1' fontWeight='bold' color='#FFF' >
+//                         {snippet?.title.slice(0,60)||demoVideoTitle.slice(0,60)}
+//                     </Typography>
+//                 </Link>
+//                 <Link to={snippet?.channelId? `/channel/${snippet?.channelId}` : demoChannelUrl } onMouseOver={(e)=>{e.target.style.color='#FFF'}} onMouseOut={(e)=>{e.target.style.color='gray'}}>
+//                     <Typography variant='subtitle2' fontWeight='bold' color='gray'sx={{width:'50%'}} >
+//                         {snippet?.channelTitle||demoChannelTitle}
+//                         <CheckCircle sx={{fontSize:'12px',color:'gray',ml:'5px'}}/>
+//                     </Typography>
+//                 </Link>
+//             </>
+//             :
+//             <>
+//                 <Skeleton sx={{ bgcolor: '#363738', zIndex:'-3' }} />
+//                 <Skeleton width="60%" sx={{ bgcolor: '#363738' }}  />
+//             </>
+//             }
+             
+//             </CardContent>
+//         </Card>
+
+
